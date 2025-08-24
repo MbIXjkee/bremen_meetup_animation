b298176f7df7937c3cfae91da40b6f3653ae84b0
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-08-24 14:05:55 +0200

    Feat: add basic parts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -44,6 +45,75 @@ class EarthAdvertising extends StatelessWidget {
               ),
             ),
         ],
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        'Our beautiful home',
+        style: Theme.of(context).textTheme.displayLarge?.copyWith(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class _FunFact extends StatelessWidget {
+  const _FunFact();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'the only place in the solar system where water can be present in all the'
+      ' three states: solid, liquid and gas.',
+      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+        color: Colors.white,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _EarthName extends StatelessWidget {
+  const _EarthName();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        'Earth',
+        style: Theme.of(context).textTheme.displayLarge?.copyWith(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+class _Earth extends StatelessWidget {
+  final Animation<double> animation;
+
+  const _Earth({required this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Lottie.asset(
+        'assets/globe.json',
+        controller: animation,
       ),
     );
   }
